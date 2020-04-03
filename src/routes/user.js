f245b34cc8a576175822478b2c8add555e0761c7
paulZuk <k4mpain@gmail.com>
2020-04-03 14:37:58 +0200

    login req validate

--- src/routes/user.js
+++ src/routes/user.js
@@ -1,26 +1,37 @@
-import { Router } from 'express';
-import User from '../models/user';
-import { registerUser } from '../controllers/UserController';
-import { body } from 'express-validator/check'
+import { Router } from "express";
+import User from "../models/user";
+import { registerUser } from "../controllers/UserController";
+import { body } from "express-validator/check";
 
 const router = Router();
 
-router.post('/', [
-    body('name').notEmpty(),
-    body('password').notEmpty(),
-    body('email')
-        .isEmail()
-        .custom((value) => {
-            return User.findOne({ email: value })
-                .then(user => {
-                    if (user) {
-                        return Promise.reject(
-                            'User already exist, please select different email and try again'
-                        );
-                    }
-                })
-        }),
-],
-    registerUser);
+router.post(
+  "/",
+  [
+    body("name")
+      .notEmpty()
+      .custom(value => {
+        return User.findOne({ login: value }).then(user => {
+          console.log(user);
+          if (user) {
+            return Promise.reject("User with this login already exist.");
+          }
+        });
+      }),
+    body("password").notEmpty(),
+    body("email")
+      .isEmail()
+      .custom(value => {
+        return User.findOne({ email: value }).then(user => {
+          if (user) {
+            return Promise.reject(
+              "User with this email already exist please select different and try again"
+            );
+          }
+        });
+      })
+  ],
+  registerUser
+);
 
 export default router;
