41329c8f06bae04497c2368cff4d578928d8a6e0
Paweł Żuk <pawel.zuk@kambu.pl>
2020-05-24 16:41:25 +0200

    delete list, edit list

--- src/controllers/ListController.js
+++ src/controllers/ListController.js
@@ -34,8 +34,29 @@ export const addList = async (req, res, next) => {
 	}
 };
 
+export const getSingleData = async (req, res, next) => {
+	const id = req.query.id;
+	try {
+		const list = await List.find({ _id: id });
+
+		res.status(200).json({
+			...list,
+		});
+	} catch (err) {
+		if (!err.statusCode) {
+			err.statusCode = 500;
+		}
+		next(err);
+	}
+};
+
 export const getList = async (req, res, next) => {
 	const user = req.user;
+
+	if (req.query.id) {
+		getSingleData(req, res, next);
+		return;
+	}
 
 	try {
 		const list = await List.find({ userId: user.id });
